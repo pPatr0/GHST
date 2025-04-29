428c8b736a52abb5e216c3ef3f3f3d7ce6e70076
patrik-sevcik <patsev01@easv365.dk>
2025-04-29 14:56:51 +0200

    feat: OrderNowSection

---
+++ components/Sections/OrderNowSection.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="lg:max-w-[1460px] max-w-[300px] mx-auto pt-28 pb-20">
+        <div class="flex flex-col items-center">
+            <h2 class="text-[#12170F] lg:text-5xl text-4xl font-bold unbounded text-center">
+                Odlište se i Vy!
+            </h2>
+            <p class="max-w-2xl py-8 text-center space">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et ex vitae nulla malesuada tempus commodo id risus. Integer vel volutpat lectus. Mauris eget felis 
+            </p>
+            <button class="px-10 py-3 bg-[#5F2C85] text-white rounded-tr-xl rounded-bl-xl unbounded">
+                Objednejte nyní
+            </button>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -4,5 +4,6 @@
       <SectionsHero/>
       <SectionsSectionOne/>
       <SectionsSectionTwo/>
+      <SectionsOrderNowSection/>
     </div>
 </template>
