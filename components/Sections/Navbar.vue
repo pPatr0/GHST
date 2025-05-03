218538385ede8792200b14c00dde36fdd6b8206b
patrik-sevcik <patsev01@easv365.dk>
2025-05-03 13:51:02 +0200

    feat: navbarLinks

--- components/Sections/Navbar.vue
+++ components/Sections/Navbar.vue
@@ -5,8 +5,20 @@
         </div>
         <div class="py-6 border-b-4 border-primary">
             <div class="max-w-[1700px] mx-auto">
-                <div class="font-sans text-4xl font-bold">
+                <div class="flex items-center justify-between">
+                    <div class="font-sans text-4xl font-bold">
                     ghst
+                    </div>
+                    <div class="space-x-8">
+                        <NuxtLin to="">Reference</NuxtLin>
+                        <NuxtLink>Katalog</NuxtLink>
+                        <NuxtLink to="/poptavka">Poptávka</NuxtLink>
+                        <NuxtLink>O nás</NuxtLink>
+                        <NuxtLink>Kontakt</NuxtLink>
+                    </div>
+                    <div>
+                        skibidi
+                    </div>
                 </div>
             </div>
         </div>
